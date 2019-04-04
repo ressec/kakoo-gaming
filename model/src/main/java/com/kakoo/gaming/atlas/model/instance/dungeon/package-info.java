991104b41f3a2f413990a4b6c4b55025b6719a71
Resse Christophe <christophe.resse@gmail.com>
2019-04-04 23:17:12 +0200

    feature(project): Create additional projects for Kakoo's Gaming.
    - Model
    - Client
    - Server

---
+++ model/src/main/java/com/kakoo/gaming/atlas/model/instance/dungeon/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2019 by Kakoo Ltd.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Kakoo Foundation Software (KFS) project which is
+ * licensed under the Apache license version 2 and use is subject to license
+ * terms. You should have received a copy of the license with the project's
+ * artifact binaries and/or sources.
+ *
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+
+/**
+ * Contains artifacts for the Kakoo's Gaming (Atlas) Model component dedicated to the dungeon entities.
+ */
+package com.kakoo.gaming.atlas.model.instance.dungeon;
